7763dc13e4fac2d1cf997c365dc7a61bb89c0b6e
Alexey Vanyukov <a_vanyukov@bk.ru>
2023-04-07 19:45:17 +0500

    fix

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,19 +4,33 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use App\Models\User;
 
 class UserController extends Controller
 {
     public function login(Request $request){
 
-        $request->validate([
-           'login' => 'required',
-           'password' => 'required'
+        $validator=Validator::make($request->all(),[
+            'login' => 'required',
+            'password' => 'required'
         ]);
 
-        if(Auth::attempt($request->only(['login','password']))){
-            return 'Успешный вход';
+        if ($validator->fails())
+            return response()->json(['errors' => [
+                'code' => 422,
+                'message' => 'Validation error',
+                'errors' => $validator->errors()
+            ]], 422);
+
+        if ($user = User::where(['login' => $request->login])->first()
+            and Hash::check($request->password, $user->password)) {
+            return response()->json([
+                'id'=>$user->id,
+                'token' => $user->generateToken()
+            ], 200);
         }
-        return 'Логин или пароль неправильный';
+
     }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -6,6 +6,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Str;
 use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
@@ -40,4 +41,12 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function generateToken()
+    {
+        $this->api_token = Str::uuid();
+        $this->save();
+        return $this->api_token;
+    }
+
 }
