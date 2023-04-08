e91fa3122660cee2bcbdada406709c5f203e75f5
Alexey Vanyukov <a_vanyukov@bk.ru>
2023-04-09 01:16:21 +0500

    fix

--- app/Http/Controllers/BoardController.php
+++ app/Http/Controllers/BoardController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Pass;
+use App\Models\WaitingDriver;
 use Illuminate\Http\Request;
 
 class BoardController extends Controller
@@ -23,6 +24,19 @@ class BoardController extends Controller
     }
 
     public function storekeeper(){
-
+        if ($drivers = WaitingDriver::where('status','Ожидание')->get()){
+            return response()->json(['data' => [
+                'code' => 200,
+                'message' => 'Ожидающие машины',
+                'drivers' => $drivers->pluck('CarNumber')->toArray(),
+                'queue' => $drivers->pluck('queue')->toArray(),
+                'point' => $drivers->pluck('point')->toArray()
+            ]], 200);
+        }else{
+            return response()->json(['errors' => [
+                'code' => 403,
+                'message' => 'Отсутсвуют машины'
+            ]], 403);
+        }
     }
 }
