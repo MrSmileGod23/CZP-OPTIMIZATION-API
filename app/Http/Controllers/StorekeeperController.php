c6832b669ba1e6f2ea1130d2a60bf7d150b7edb3
Alexey Vanyukov <a_vanyukov@bk.ru>
2023-04-09 05:38:35 +0500

    add algorithm

--- app/Http/Controllers/StorekeeperController.php
+++ app/Http/Controllers/StorekeeperController.php
@@ -8,11 +8,52 @@ use Illuminate\Http\Request;
 class StorekeeperController extends Controller
 {
     public function index(){
+        $smallTrucks = [];
+        $bigTrucks = [];
+        $trucks = WaitingDriver::all();
+        // Разбиваем грузовики на две части: маленькие и большие
+        foreach ($trucks as $truck) {
+            if ($truck->ProductVolume <= 15000) {
+                $smallTrucks[] = $truck;
+            } else {
+                $bigTrucks[] = $truck;
+            }
+        }
+
+        // Сортируем каждую часть по тоннажу
+        usort($smallTrucks, function ($a, $b) {
+            return $a->weight <=> $b->weight;
+        });
+
+        usort($bigTrucks, function ($a, $b) {
+            return $a->weight <=> $b->weight;
+        });
+
+        // Собираем новый массив грузовиков
+        $sortedTrucks = [];
+        $smallCount = count($smallTrucks);
+        $bigCount = count($bigTrucks);
+        $i = 0;
+        $j = 0;
+
+        while ($i < $smallCount && $j < $bigCount) {
+            $sortedTrucks[] = $smallTrucks[$i];
+            $sortedTrucks[] = $smallTrucks[$i + 1];
+            $sortedTrucks[] = $bigTrucks[$j];
+            $i += 2;
+            $j++;
+        }
+
+        // Добавляем необработанные маленькие грузовики
+        while ($i < $smallCount) {
+            $sortedTrucks[] = $smallTrucks[$i];
+            $i++;
+        }
         if ($drivers = WaitingDriver::all()){
             return response()->json(['data' => [
                 'code' => 200,
                 'message' => 'Все машины',
-                'drivers' => $drivers
+                'drivers' => $sortedTrucks
             ]], 200);
         }
     }
--- app/Models/WaitingDriver.php
+++ app/Models/WaitingDriver.php
@@ -22,4 +22,50 @@ class WaitingDriver extends Model
         'queue',
         'point'
     ];
+    public function sortTrucksByWeight(array $trucks): array
+    {
+        $smallTrucks = [];
+        $bigTrucks = [];
+
+        // Разбиваем грузовики на две части: маленькие и большие
+        foreach ($trucks as $truck) {
+            if ($truck->ProductVolume <= 15000) {
+                $smallTrucks[] = $truck;
+            } else {
+                $bigTrucks[] = $truck;
+            }
+        }
+
+        // Сортируем каждую часть по тоннажу
+        usort($smallTrucks, function ($a, $b) {
+            return $a->weight <=> $b->weight;
+        });
+
+        usort($bigTrucks, function ($a, $b) {
+            return $a->weight <=> $b->weight;
+        });
+
+        // Собираем новый массив грузовиков
+        $sortedTrucks = [];
+        $smallCount = count($smallTrucks);
+        $bigCount = count($bigTrucks);
+        $i = 0;
+        $j = 0;
+
+        while ($i < $smallCount && $j < $bigCount) {
+            $sortedTrucks[] = $smallTrucks[$i];
+            $sortedTrucks[] = $smallTrucks[$i + 1];
+            $sortedTrucks[] = $bigTrucks[$j];
+            $i += 2;
+            $j++;
+        }
+
+        // Добавляем необработанные маленькие грузовики
+        while ($i < $smallCount) {
+            $sortedTrucks[] = $smallTrucks[$i];
+            $i++;
+        }
+
+        return $sortedTrucks;
+    }
 }
